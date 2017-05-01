0fc47667bb16ac48116c58bda4bb01b85033ae3d
drzly777 <andrew.zheng725@gmail.com>
2017-05-01 15:29:51 -0400

    Added sql functions

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "M", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "M", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Seargeant Brown", 7, "M", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 9, "M", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 10, "M", "brown", "sweet", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3,  "M", "brown", "sweet", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4, "M", "brown", "sweet", 1);
